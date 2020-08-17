0a1986612f67987d543c3daa7c2a9b31a9e466a3
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:29:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,4 @@
 # Write your code here.
-katz_deli = []
-def line(customer)
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  end
-end
+def line
 
-def take_a_number(array, new_customer)
-  puts "Welcome, #{new_customer}. You are number #{array.size + 1}"
 end
