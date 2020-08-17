350b990a88657399b6144a21624d9cca1118e603
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:54:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(array, name)
   array << name
-  puts "Welcome, #{name}. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 
 end
