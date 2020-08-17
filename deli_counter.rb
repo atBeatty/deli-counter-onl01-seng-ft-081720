4ca8ca00d605373837643697b0176b3f8a0ead84
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:57:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,12 @@ end
 def take_a_number(array, name)
   array << name
   puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    puts "Currently serving #{array}"
 
 end
