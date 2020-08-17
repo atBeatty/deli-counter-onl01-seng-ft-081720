b6b78206bcc13ce766d24fd8ee8e235563aaef2f
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:44:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each_with_index do |value, index|
-    puts "The line is currently: #{index + 1}"
-
+      puts "The line is currently: #{index + 1}"
+    end
   end
 
 end
