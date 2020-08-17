bfca668521c3242950f6dc4bf9e1f86e59dcbfc3
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:42:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 def line(array)
-  output_message = "The line is currently"
+
   if array.size == 0
-    output_message << " empty."
+    puts "The line is currently empty."
   else
-    array.each do |customer|
-    output_message << " #{customer.index + 1}. #{customer}"
+
+    puts "The line is currently: "
+
   end
 
   end
-puts output_message
 end
