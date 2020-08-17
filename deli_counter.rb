2d8f5770553eaaa48fe11db88fd00c94f472af3c
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:38:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(array)
   output_message = "The line is currently"
   if array.size == 0
-    output_message + " empty."
+    output_message << " empty."
   else
     array.each do |customer|
     output_message += "#{i} customer"
