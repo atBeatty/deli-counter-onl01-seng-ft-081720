050bca22d453363add6a34d883793649161729ec
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 21:58:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(array)
   if array.size == 0
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
     array.pop()
